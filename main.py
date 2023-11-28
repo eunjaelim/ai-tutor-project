540bed120f5989558c5ec59cc7a76760c3bb19e4
Eunjnae Lim <99618295+eunjaelim@users.noreply.github.com>
2023-11-28 11:44:23 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import googletrans
 from langchain.llms import CTransformers
-from streamlit.scriptrunner import add_script_run_ctx
 
 translator = googletrans.Translator()
 
