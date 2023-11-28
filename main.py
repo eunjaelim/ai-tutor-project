2d1d79f73b779289ebdff3326977f157815ab845
Eunjnae Lim <99618295+eunjaelim@users.noreply.github.com>
2023-11-28 11:55:09 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import streamlit as st
-import googletrans
+from googletrans import Translator
 from langchain.llms import CTransformers
 
-translator = googletrans.Translator()
+
+translator = Translator()
+
 
 llm = CTransformers(
     model="llama-2-7b-chat.ggmlv3.q2_K (2).bin",
@@ -16,6 +18,6 @@ content = st.text_input("답변을 요청하세요!")
 if st.button('요청하기'):
     with st.spinner('잠시만 기다려주세요...'):
         content_en = translator.translate(content, dest="en", src='auto').text
-        result = llm.predict("Explain about" + content_en + " to elementary school students:")
+        result = llm.predict("Explain about" + content_en + " to elementary school students easily:")
         translation = translator.translate(result, src='en', dest='ko').text  # 영어에서 한국어로 번역
         st.write(translation)  # 번역된 결과 출력
